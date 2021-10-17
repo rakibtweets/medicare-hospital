368b6fc69f638df9cb75e52bf6e45fb16568f582
rakibtweets <rakibhasanroky0@gmail.com>
2021-10-18 02:54:09 +0600

    firebase initialize Authentication

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCUkWZTuGP4cA2Cc41T9TCY1DbRN6TRHXY',
+  authDomain: 'medicareplus-responsive.firebaseapp.com',
+  projectId: 'medicareplus-responsive',
+  storageBucket: 'medicareplus-responsive.appspot.com',
+  messagingSenderId: '1006069360323',
+  appId: '1:1006069360323:web:c4f2b3aecd375f09fca476',
+};
+export default firebaseConfig;
