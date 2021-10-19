3a035afe01e495b0aa7e4aa78e008b9ed1ae1552
rakibtweets <rakibhasanroky0@gmail.com>
2021-10-19 20:37:02 +0600

    highlited features added

---
+++ src/Components/FeaturedService/FeaturedService.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './FeaturedService.css';
+import { FaBalanceScale, FaNotesMedical, FaUserMd } from 'react-icons/fa';
+
+const FeaturedService = () => {
+  return (
+    <div className="categories-container text-center container mx-auto mt-4 row gx-3">
+      <h2 className="text-center fw-bolder py-2">BEST HOSPITAL IN TOWN</h2>
+      <h5 className=" text-secondary text-center mb-5">
+        When It Comes to Health Care
+      </h5>
+      <div className=" features  col-md-4">
+        <div className="text-center card p-3">
+          <div>
+            <FaUserMd className=" icon text-primary py-3" />
+          </div>
+          <div className="title text-center">
+            <h6 className=" fw-bold mt-1">HealthCare Professionals</h6>
+            <p className="text-seconday">
+              We have Professionals to care your health 24hrs
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className=" categories col-md-4">
+        <div className="text-center card p-3">
+          <div>
+            <FaBalanceScale className=" icon text-primary py-3" />
+          </div>
+          <div className="title">
+            <h6 className="fw-bold mt-1">Medical Excellence</h6>
+            <p className="text-seconday">
+              Our Doctors are well certified and have achive medical experiences
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className=" categories col-md-4">
+        <div className=" text-center card p-3">
+          <div>
+            <FaNotesMedical className=" icon text-primary py-3" />
+          </div>
+          <div className="title">
+            <h6 className="fw-bold mt-1">Latest Technologies</h6>
+            <p className="text-seconday">
+              We have latest Technologies available in the World
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedService;
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Doctors from '../Doctors/Doctors';
+import FeaturedService from '../FeaturedService/FeaturedService';
 import Services from '../Services/Services';
 import './Main.css';
 
@@ -8,6 +9,7 @@ const Main = () => {
   return (
     <div>
       <Banner />
+      <FeaturedService />
       <Services />
       <Doctors />
     </div>
